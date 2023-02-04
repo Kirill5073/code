ce5bd23f5f675498185a5cf1313d1a4106ebe1ee
oxygen <oxygen@trafficstars.com>
2023-02-04 19:10:48 +0200

    initial commit

---
+++ popup.js
@@ -0,0 +1,52 @@
+function initPopup({ id, crossId, time = 2, moreThan = 768, lessThan }) {
+  const modal = document.getElementById(id);
+  const closeButton = document.getElementById(crossId);
+
+  function countDown() {
+    closeButton.innerHTML = time;
+
+    const intervalId = setInterval(() => {
+      if (time === 1) {
+        closeButton.innerHTML = "&times;";
+        clearInterval(intervalId);
+        closeButton.removeAttribute("disabled");
+      } else {
+        time = time - 1;
+        closeButton.innerText = time;
+      }
+    }, 1000);
+  }
+
+  if (
+    (window.innerWidth >= moreThan && !lessThan) ||
+    (window.innerWidth >= moreThan && window.innerWidth < lessThan)
+  ) {
+    setTimeout(() => {
+      modal.classList.add(`${id}__show`);
+      countDown();
+    }, 1000);
+  }
+
+  closeButton.onclick = function () {
+    modal.classList.remove(`${id}__show`);
+  };
+}
+
+window.onload = () => {
+  initPopup({
+    id: "ad_custom_popup",
+    crossId: "ad_custom_popup__cross",
+    moreThan: 786,
+    lessThan: 1786,
+  });
+  initPopup({
+    id: "ad_custom_popup-left",
+    crossId: "ad_custom_popup-left__cross",
+    moreThan: 1786,
+  });
+  initPopup({
+    id: "ad_custom_popup-right",
+    crossId: "ad_custom_popup-right__cross",
+    moreThan: 1786,
+  });
+};
